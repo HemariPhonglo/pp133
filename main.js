3ceb0f14795ab70b5b625a5cb87cba691de0e957
HemariPhonglo <95082407+HemariPhonglo@users.noreply.github.com>
2024-01-03 11:44:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+img="";
+status="";
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function preload(){
+    img = loadImage('bed.jpg');
+    }
+    function modelLoaded() {
+        console.log("Model Loaded!");
+        status = true;
+        objectDetector.detect(img, gotResult);
+    
+    }
+    function gotResult(error, results){
+        if(error) {
+            console.log(error);
+        }
+        console.log(results);
+    }
